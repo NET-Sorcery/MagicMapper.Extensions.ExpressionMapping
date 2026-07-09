76f6de620ee59c533dc0faa3a90da1f522ffee61
Andrii Kurdiumov <kant2002@gmail.com>
2026-07-09 15:55:47 +0200

    Rename projects to have MagicMapper in the name

--- src/MagicMapper.Extensions.ExpressionMapping/Properties/Resources.Designer.cs
+++ src/MagicMapper.Extensions.ExpressionMapping/Properties/Resources.Designer.cs
@@ -39,7 +39,7 @@ namespace AutoMapper.Extensions.ExpressionMapping.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("AutoMapper.Extensions.ExpressionMapping.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("MagicMapper.Extensions.ExpressionMapping.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
